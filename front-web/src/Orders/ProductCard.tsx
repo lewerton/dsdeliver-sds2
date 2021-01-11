aeff98058099b1088abd39b8950abee3604233d1
lewerton <lewerton95@gmail.com>
2021-01-11 05:08:41 -0300

    Finish front-web

--- front-web/src/Orders/ProductCard.tsx
+++ front-web/src/Orders/ProductCard.tsx
@@ -1,23 +1,16 @@
+import { formatePrice } from "./helpers";
 import {ReactComponent as Pizza} from "./pizza.svg";
 import { Product } from "./types";
 
 type Props = {
     product: Product;
+    onSelectProduct: (product: Product) => void;
+    isSelected: boolean;
 }
 
-function formatePrice(price: number) {
-    const formatter = new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
-        minimumFractionDigits: 2
-    });
-
-    return formatter.format(price)
-}
-
-function ProductCard({ product }: Props){
+function ProductCard({ product, onSelectProduct, isSelected }: Props){
     return (
-        <div className="order-card-container">
+        <div className={`order-card-container ${isSelected ? 'selected' : ''}`} onClick={() => onSelectProduct(product)}>
             <h3 className="order-card-title">
                 {product.name}
             </h3>
